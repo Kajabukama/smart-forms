91d7df7407411615c04ac101fb196aad70c4c06b
Kajabukama <kajabukama@Kajabukamas-MacBook-Pro.local>
2017-05-21 17:24:06 +0300

    Added a check if the form field is empty or not

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,7 +1,12 @@
 define(['jquery'], function(){
-	
+
 	$('#contactForm').on('submit', function(){
 		var fname = $('#fname').val()
-		alert('submitted name : '+ fname);
+
+		if (fname === '') {
+			alert('the form field is empty');
+		}else{
+			alert('Your name is : ' + fname.toUpperCase());
+		}
 	})
 })
