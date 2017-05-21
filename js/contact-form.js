112555c8486cdb05e8fac23ad0ef48791553fe67
Focus Joseph <focusjoseph12@gmail.com>
2017-05-21 22:18:33 +0300

    Implemented input check and email pattern check

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,12 +1,51 @@
-define(['jquery'], function(){
+define(['jquery'], function() {
 
-	$('#contactForm').on('submit', function(){
-		var fname = $('#fname').val()
+    function validateEmail(email) {
+        var pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+        return $.trim(email).match(pattern) ? true : false;
+    }
 
-		if (fname === '') {
-			alert('the form field is empty');
-		}else{
-			alert('Your name is : ' + fname.toUpperCase());
-		}
-	})
+    function check(input) {
+        return (input != '') ? true : false;
+    }
+
+    var errors = [];
+    var data = {};
+
+    var output = $('.errors');
+
+
+
+    $('#contact').on('submit', function(ev) {
+
+        var email = $('#email').val();
+        var fullname = $('#fullname').val();
+        var mobile = $('#mobile').val();
+        var message = $('#message').val();
+
+        if (!validateEmail(email)) {
+            errors.push('Email is not valid')
+        }
+
+        if (!check(fullname) && !check(message) && !check(mobile) && !check(email)) {
+            errors.push('You must provide a fullname');
+            errors.push('You must provide a message');
+            errors.push('You must provide a mobile phone number');
+            errors.push('Email field is empty')
+        }
+
+        data = { 'email': email, 'fullname': fullname, 'mobile': mobile, 'message': message }
+
+        if (errors.length == 0) {
+            console.log('no errors');
+            console.log(data);
+        } else {
+            console.log(errors);
+        }
+
+
+
+        ev.preventDefault()
+    });
+
 })
